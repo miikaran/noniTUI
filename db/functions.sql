19e77079cdcab01628260c444d28127f7a7e61eb
miikaran <42628@edu.raseko.fi>
2025-03-04 20:29:23 +0200

    Added triggers for DELETE and UPDATE operations, and updated notifier to send payload based on the operation type

--- db/functions.sql
+++ db/functions.sql
@@ -4,18 +4,27 @@ RETURNS TRIGGER AS
 $$
 DECLARE
     channel_name TEXT;
+    payload TEXT;
 BEGIN
-    -- Publish inserts on different tables to different named channels
+    -- Publish updates on different tables to different named channels
     -- For example:
     --    Changes on projects table -> publish to projects_channel_<project_id>
     --    Changes on messages table -> publish to messages_channel_<project_id>
     --    etc...
     --    Makes it easier to listen and process data from different tables
     channel_name := format('%I_channel_%s', TG_TABLE_NAME, COALESCE(NEW.project_id, 'public'));
+    IF TG_OP = 'INSERT' THEN
+        payload := json_build_object('operation', 'INSERT', 'table', TG_TABLE_NAME, 'new_data', row_to_json(NEW))::text;
+    ELSIF TG_OP = 'UPDATE' THEN
+        payload := json_build_object('operation', 'UPDATE', 'table', TG_TABLE_NAME, 'updated_data', row_to_json(NEW))::text;
+    ELSIF TG_OP = 'DELETE' THEN
+        payload := json_build_object('operation', 'DELETE', 'table', TG_TABLE_NAME, 'old_data', row_to_json(OLD))::text;
+    END IF;
+    -- Send updated data to project specific channel
     EXECUTE format(
         'NOTIFY %I, %L', 
-        format('project_channel_%s', NEW.project_id),
-        row_to_json(NEW)::text
+        channel_name,
+        payload
     );
     RETURN NEW;
 END;
--- db/triggers.sql
+++ db/triggers.sql
@@ -1,18 +1,58 @@
--- Trigger: trigger_notify - Execute notifier for each new row to x-table
+-- Trigger: trigger_notify - Execute notifier for each change to x-table
+
+-- TABLE: projects
 CREATE TRIGGER trigger_notify_projects
 AFTER INSERT
 ON projects
 FOR EACH ROW
 EXECUTE PROCEDURE notify_changes_after_insert();
 
+CREATE TRIGGER trigger_notify_projects
+AFTER UPDATE
+ON projects
+FOR EACH ROW
+EXECUTE PROCEDURE notify_changes_after_insert();
+
+CREATE TRIGGER trigger_notify_projects
+AFTER DELETE
+ON projects
+FOR EACH ROW
+EXECUTE PROCEDURE notify_changes_after_insert();
+
+-- TABLE: tasks
 CREATE TRIGGER trigger_notify_tasks
 AFTER INSERT
 ON tasks
 FOR EACH ROW
 EXECUTE PROCEDURE notify_changes_after_insert();
 
+CREATE TRIGGER trigger_notify_tasks
+AFTER UPDATE
+ON tasks
+FOR EACH ROW
+EXECUTE PROCEDURE notify_changes_after_insert();
+
+CREATE TRIGGER trigger_notify_tasks
+AFTER DELETE
+ON tasks
+FOR EACH ROW
+EXECUTE PROCEDURE notify_changes_after_insert();
+
+-- TABLE: messages
 CREATE TRIGGER trigger_notify_messages
 AFTER INSERT
+ON messages
+FOR EACH ROW
+EXECUTE PROCEDURE notify_changes_after_insert();
+
+CREATE TRIGGER trigger_notify_messages
+AFTER UPDATE
+ON messages
+FOR EACH ROW
+EXECUTE PROCEDURE notify_changes_after_insert();
+
+CREATE TRIGGER trigger_notify_messages
+AFTER DELETE
 ON messages
 FOR EACH ROW
 EXECUTE PROCEDURE notify_changes_after_insert();
