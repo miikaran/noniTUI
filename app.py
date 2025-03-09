abef6c6034b0cee053f11a82e83869e4541dc22f
evnct <77973084+evnct@users.noreply.github.com>
2025-03-09 02:27:02 +0200

    Changed Session to SessionScreen

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from textual.app import App, ComposeResult
 from textual.widgets import Static
-from screens.session import Session
+from screens.session import SessionScreen
+from textual import events
 
 
 class Noni(App):
-    
+  
     # Session screen will always be the first screen on launch
     def on_mount(self) -> None:
-        self.push_screen(Session())
+        self.theme = "tokyo-night"
+        self.push_screen(SessionScreen())
 
 if __name__ == "__main__":
     app = Noni()
