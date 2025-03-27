19a12b14d9493ff6c45751d826aa885c1508ef14
evnct <77973084+evnct@users.noreply.github.com>
2025-03-27 15:55:38 +0200

    Added Chat-pane (without input), task-pane, stat-pane and info-pane. WIP.
    Some UI to work with, WIP, will continue later. I will start splitting
    things into their own files and such ones the general layout is
    complete.

--- screens/management.py
+++ screens/management.py
@@ -1,10 +1,10 @@
-"""
-Management screen is the screen where all the project management happens.
-Modals are relevant in this screen.
-"""
-from textual.app import ComposeResult, RenderResult
+from textual.app import ComposeResult
 from textual.screen import Screen
-from textual.widgets import Static
+from textual.containers import Container, Horizontal, VerticalScroll
+from textual.widgets import Static, TabbedContent, TabPane
+
+from version import __version__
+from widgets.message import Message
 
 class ManagementScreen(Screen):
     def __init__(self, project_uuid: str, new=True):
@@ -12,7 +12,39 @@ class ManagementScreen(Screen):
         self.project_uuid = project_uuid
         self._running = False
 
+    CSS_PATH = [
+        "../styles/management.tcss",
+        "../styles/message.tcss",
+    ]
+
     def compose(self) -> ComposeResult:
-        yield Static(f"Management Screen for Project: {self.project_uuid}")
-
-
+        with Container(id="app-grid"):
+            with Container(id="information-bar"):
+                yield Horizontal(
+                    Static(f"Noni {__version__}", id="noni-text")
+                    # Add other information, e.g., session users.
+                )
+            with VerticalScroll(id="chat-left-pane"):
+                yield Message()
+            with Container(id="tasks-top-right"):
+                with TabbedContent(initial="backlog-tabpane"):
+                    with TabPane("Backlog", id="backlog-tabpane"):
+                        yield VerticalScroll(
+                                Static("Implement chat | HIGH", classes="task"),
+                                Static("Implement chat | HIGH", classes="task"),
+                                Static("Implement chat | HIGH", classes="task"),
+                        )
+                    with TabPane("Todo", id="todo-tabpane"):
+                        yield Static("Todo")
+                    with TabPane("In-Progress", id="in-progress-tabpane"):
+                        yield Static("In-Progress")
+                    with TabPane("Done", id="done-tabpane"):
+                        yield Static("Done")
+            with Container(id="stats-bottom-right"):
+               with Container(id="stat-days"):
+                    yield Horizontal(
+                        Static("1 Done"),
+                        Static("3 Updated"),
+                        Static("7 Created"),
+                        Static("0 Due"),
+                    )
